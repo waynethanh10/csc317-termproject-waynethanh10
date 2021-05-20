a483f974a47171fac27eb50e66a8a6a3ef95a96e
waynethanh10 <78188819+waynethanh10@users.noreply.github.com>
2021-05-20 12:00:14 +0530

    Add files via upload

--- project.sql
+++ project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 18, 2021 at 01:10 PM
+-- Generation Time: May 20, 2021 at 06:29 AM
 -- Server version: 10.3.28-MariaDB
 -- PHP Version: 7.4.15
 
@@ -24,9 +24,31 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `text` varchar(500) NOT NULL,
+  `created` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`id`, `post_id`, `text`, `created`) VALUES
+(1, 2, 'sdgdgd', '2021-05-20 11:11:56');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `posts`
 --
 
+DROP TABLE IF EXISTS `posts`;
 CREATE TABLE `posts` (
   `id` int(11) NOT NULL,
   `title` varchar(100) NOT NULL,
@@ -40,7 +62,8 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`id`, `title`, `description`, `img`, `created`) VALUES
-(1, 'WebGL', 'tsrgg', 'extjs.png', '2021-05-18 18:19:11');
+(1, 'WebGL', 'sdfasdfd', 'extjs.png', '2021-05-19 17:56:50'),
+(2, 'WebGL', 'gdfsd', 'extjs.png', '2021-05-19 18:11:39');
 
 -- --------------------------------------------------------
 
@@ -48,6 +71,7 @@ INSERT INTO `posts` (`id`, `title`, `description`, `img`, `created`) VALUES
 -- Table structure for table `users`
 --
 
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `email` varchar(300) NOT NULL,
@@ -61,11 +85,18 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `email`, `username`, `password`, `created`) VALUES
-(1, 'admin@localhost.local', 'admin', '7c4a8d09ca3762af61e59520943dc26494f8941b', '2021-05-18 18:18:35');
+(1, 'admin@localhost.local', 'admin', '7c4a8d09ca3762af61e59520943dc26494f8941b', '2021-05-19 17:56:18'),
+(2, 'admin@localhost.local', 'admin', '7c4a8d09ca3762af61e59520943dc26494f8941b', '2021-05-19 18:11:03');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `posts`
@@ -84,16 +115,22 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
